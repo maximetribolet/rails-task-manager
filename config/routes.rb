6bcd8cb47b75ff54167e40fbcbe61debad1f24d2
Maxime Tribolet <maxime.tribolet@gmail.com>
2022-11-15 16:44:38 +0100

    fini

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  delete "tasks/:id", to: "tasks#destroy", as: :delete_task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  get "tasks/new", to: "tasks#new",  as: :new_task
+  post "tasks", to: "tasks#create"
+  get "/tasks", to: "tasks#index",  as: :task_list
+  get "tasks/:id", to: "tasks#show", as: :task
+
 end
